027002e613981014b0862a95e743dc6cb0d897bb
mo7amedehab97 <gazacityface@gmail.com>
2022-04-07 00:21:44 +0300

    create delete task feature relatees #7

--- src/components/Forms/TopForms.jsx
+++ src/components/Forms/TopForms.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-const TopForms = ({toggleBtn, openAdd}) => {
-
+const TopForms = ({ toggleBtn, openAdd }) => {
   return (
-    <div className='sec-top'>
-      <h3 className='sec-title'>My Tasks</h3>
-      <button className={`btn open-btn ${openAdd ? 'close' : ''}`} onClick={toggleBtn}>
-        {
-          openAdd
-          ?
-          <div className='close-span'>
-            <span className='ri-close-line icon'></span> Cancel
+    <div className="sec-top">
+      <h3 className="sec-title">My Tasks</h3>
+      <button
+        className={`btn open-btn ${openAdd ? 'close' : ''}`}
+        onClick={toggleBtn}
+      >
+        {openAdd ? (
+          <div className="close-span">
+            <span className="ri-close-line icon"></span> Cancel
           </div>
-          :
-          <div className='add-span'>
-            <span className='ri-add-fill icon'></span> Add Task
+        ) : (
+          <div className="add-span">
+            <span className="ri-add-fill icon"></span> Add Task
           </div>
-        }
-        
+        )}
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default TopForms;
+export default TopForms
--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -1,43 +1,46 @@
-import React from 'react';
+import React from 'react'
 
-const TaskList = ({ tasks }) => {
-  let res = tasks;
-  console.log(tasks);
+const TaskList = ({ tasks, deleteHandler }) => {
+  let res = tasks
   return (
     <>
-      <ul className='tasks-list'>
+      <ul className="tasks-list">
         {res
           .sort((a, b) => a.id - b.id)
           .map((task) => (
-            <li className='single-task' data-id={task.id} key={task.id}>
-              <button className='done-btn' data-id='status'>
-                <span className='not-done-icon ri-checkbox-blank-circle-line icon' />
-                <span className='done-icon ri-checkbox-circle-line icon' />
+            <li className="single-task" data-id={task.id} key={task.id}>
+              <button className="done-btn" data-id="status">
+                <span className="not-done-icon ri-checkbox-blank-circle-line icon" />
+                <span className="done-icon ri-checkbox-circle-line icon" />
               </button>
-              <div className='task-details'>
-                <p className='task-name'>{task.title}</p>
-                <div className='day-time'>
-                  <p className='day'>
-                    <span className='ri-calendar-line icon' />
-                    <span className='value day'>{task.day}</span>
+              <div className="task-details">
+                <p className="task-name">{task.title}</p>
+                <div className="day-time">
+                  <p className="day">
+                    <span className="ri-calendar-line icon" />
+                    <span className="value day">{task.day}</span>
                   </p>
-                  <p className='time'>
-                    <span className='ri-time-line icon' />
-                    <span className='value day'>{task.time}</span>
+                  <p className="time">
+                    <span className="ri-time-line icon" />
+                    <span className="value day">{task.time}</span>
                   </p>
                 </div>
               </div>
-              <button className='edit-btn' data-id='edit'>
-                <span className='trash-icon ri-pencil-line icon' />
+              <button className="edit-btn" data-id="edit">
+                <span className="trash-icon ri-pencil-line icon" />
               </button>
-              <button className='trash-btn' data-id='delete'>
-                <span className='trash-icon ri-delete-bin-6-line icon' />
+              <button
+                className="trash-btn"
+                data-id="delete"
+                onClick={() => deleteHandler(task.id)}
+              >
+                <span className="trash-icon ri-delete-bin-6-line icon" />
               </button>
             </li>
-            ))}
+          ))}
       </ul>
     </>
-  );
-};
+  )
+}
 
-export default TaskList;
+export default TaskList
