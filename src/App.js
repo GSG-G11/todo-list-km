8a1be9c00018755b9e5fa2cd7b4fbf4a5035a887
karam-zomlut <zomlutk@gmail.com>
2022-04-07 01:27:38 +0300

    Finish All App Relates #9

--- src/App.js
+++ src/App.js
@@ -10,59 +10,86 @@ import './App.css'
 import 'remixicon/fonts/remixicon.css'
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       taskTitle: '',
       taskDay: '',
       taskTime: '',
       openAdd: false,
+      editMode: false,
+      thisTask: '',
       tasks: [],
-    }
+    };
   }
 
   toggleBtn = (e) => {
     this.setState((prevState) => {
-      return { openAdd: !prevState.openAdd }
-    })
+      return { openAdd: !prevState.openAdd, editMode: false, taskTime: '', taskTitle: '', taskDay: ''};
+    });
+  };
+
+  openEdit = (task) => {
+    this.setState({openAdd: true, editMode: true, thisTask: task.id,  taskTitle: task.title, taskDay: task.day, taskTime: task.time})
+    // this.editHandler(task.id);
   }
 
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
-  deleteHandler = (taskId) => {
-    this.setState((prevState)=>{
-      return {tasks:prevState.tasks.filter((task) => task.id !== taskId)}
-    })
-  }
-  addTask = (title, day, time)=>{
+  addTask = (title, day, time) => {
     const task = {
       id: Date.now(),
       title: title,
       day: day,
       time: time,
       isDone: false,
-    }
+    };
     this.setState((prevState) => {
       return {
         tasks: [...prevState.tasks, task],
         taskTitle: '',
         taskDay: '',
         taskTime: '',
-      }
-    })
-    this.toggleBtn()
-  }
+      };
+    });
+    this.toggleBtn();
+  };
+
+  completeHandler = (taskId) => {
+    let tasks = this.state.tasks;
+    tasks.forEach((task) =>
+      task.id === taskId ? (task.isDone = !task.isDone) : true
+    );
+    this.setState({ tasks: tasks });
+  };
+  
+  editHandler = (taskId) => {
+    let {tasks, taskTitle, taskDay, taskTime} = this.state
+    let thisTask = tasks.filter((task) => task.id === taskId);
+    thisTask[0].title = taskTitle;
+    thisTask[0].day = taskDay;
+    thisTask[0].time = taskTime;
+    console.log(thisTask);
+    this.setState({ tasks: tasks, openAdd: false, editMode: false });
+  };
+
+  deleteHandler = (taskId) => {
+    this.setState((prevState) => {
+      return { tasks: prevState.tasks.filter((task) => task.id !== taskId) };
+    });
+  };
 
   render() {
-    const { taskTitle, taskDay, taskTime, openAdd, tasks } = this.state
+    const { taskTitle, taskDay, taskTime, openAdd, tasks, editMode, thisTask } =
+      this.state;
     return (
-      <div className="container">
+      <div className='container'>
         <HeaderPage />
-        <main className="page-main">
-          <section className="add-sec">
+        <main className='page-main'>
+          <section className='add-sec'>
             <TopForms toggleBtn={this.toggleBtn} openAdd={openAdd} />
 
             <AddForm
@@ -70,19 +97,27 @@ class App extends Component {
               handleChange={this.handleChange}
               addTask={this.addTask}
               openAdd={openAdd}
+              editMode={editMode}
+              editHandler={this.editHandler}
+              thisTask={thisTask}
             />
           </section>
 
-          <section className="view-sec">
+          <section className='view-sec'>
             {tasks.length ? (
-              <TaskList tasks={tasks} deleteHandler={this.deleteHandler} />
+              <TaskList
+                tasks={tasks}
+                deleteHandler={this.deleteHandler}
+                completeHandler={this.completeHandler}
+                openEdit={this.openEdit}
+              />
             ) : (
               <NoTasks />
             )}
           </section>
         </main>
       </div>
-    )
+    );
   }
 }
 
--- src/components/Tasks/TaskList.jsx
+++ src/components/Tasks/TaskList.jsx
@@ -1,46 +1,54 @@
 import React from 'react'
 
-const TaskList = ({ tasks, deleteHandler }) => {
-  let res = tasks
+const TaskList = ({ tasks, deleteHandler, openEdit, completeHandler }) => {
+  let res = tasks;
   return (
     <>
-      <ul className="tasks-list">
+      <ul className='tasks-list'>
         {res
           .sort((a, b) => a.id - b.id)
           .map((task) => (
-            <li className="single-task" data-id={task.id} key={task.id}>
-              <button className="done-btn" data-id="status">
-                <span className="not-done-icon ri-checkbox-blank-circle-line icon" />
-                <span className="done-icon ri-checkbox-circle-line icon" />
+            <li
+              className={`single-task ${task.isDone ? 'completed' : ''}`}
+              data-id={task.id}
+              key={task.id}>
+              <button
+                className='done-btn'
+                data-id='status'
+                onClick={() => completeHandler(task.id)}>
+                <span className='not-done-icon ri-checkbox-blank-circle-line icon' />
+                <span className='done-icon ri-checkbox-circle-line icon' />
               </button>
-              <div className="task-details">
-                <p className="task-name">{task.title}</p>
-                <div className="day-time">
-                  <p className="day">
-                    <span className="ri-calendar-line icon" />
-                    <span className="value day">{task.day}</span>
+              <div className='task-details'>
+                <p className='task-name'>{task.title}</p>
+                <div className='day-time'>
+                  <p className='day'>
+                    <span className='ri-calendar-line icon' />
+                    <span className='value day'>{task.day}</span>
                   </p>
-                  <p className="time">
-                    <span className="ri-time-line icon" />
-                    <span className="value day">{task.time}</span>
+                  <p className='time'>
+                    <span className='ri-time-line icon' />
+                    <span className='value day'>{task.time}</span>
                   </p>
                 </div>
               </div>
-              <button className="edit-btn" data-id="edit">
-                <span className="trash-icon ri-pencil-line icon" />
+              <button
+                className='edit-btn'
+                data-id='edit'
+                onClick={() => openEdit(task)}>
+                <span className='trash-icon ri-pencil-line icon' />
               </button>
               <button
-                className="trash-btn"
-                data-id="delete"
-                onClick={() => deleteHandler(task.id)}
-              >
-                <span className="trash-icon ri-delete-bin-6-line icon" />
+                className='trash-btn'
+                data-id='delete'
+                onClick={() => deleteHandler(task.id)}>
+                <span className='trash-icon ri-delete-bin-6-line icon' />
               </button>
             </li>
           ))}
       </ul>
     </>
-  )
-}
+  );
+};
 
 export default TaskList
